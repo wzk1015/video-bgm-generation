ccc04f8a09b0efa78a3cd8f77b5fd70a1e3121ca
Zhaokai Wang <847791804@qq.com>
2021-09-10 16:20:34 +0800

    add control error

--- src/gen_midi_conditional.py
+++ src/gen_midi_conditional.py
@@ -23,13 +23,62 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '7'
 print("device", os.environ['CUDA_VISIBLE_DEVICES'])
 
 
+def cal_control_error(err_note_number_list, err_beat_number_list):
+    # number of notes per simu-note
+    # o_dens: 1, 7868576 / 14468678, 54.38%
+    # o_dens: 2, 3334852 / 14468678, 23.05%
+    # o_dens: 3, 1934401 / 14468678, 13.37%
+    # o_dens: 4, 835012 / 14468678, 5.77%
+    # o_dens: 5, 308613 / 14468678, 2.13%
+    # o_dens: 6, 130653 / 14468678, 0.90%
+    # o_dens: 7, 36176 / 14468678, 0.25%
+    # o_dens: 8, 13118 / 14468678, 0.09%
+    # o_dens: 9, 4381 / 14468678, 0.03%
+    # o_dens: 10, 1694 / 14468678, 0.01%
+    # o_dens: 11, 549 / 14468678, 0.00%
+    # o_dens: 12, 383 / 14468678, 0.00%
+    # o_dens: 13, 92 / 14468678, 0.00%
+    # o_dens: 14, 33 / 14468678, 0.00%
+    # o_dens: 15, 59 / 14468678, 0.00%
+    # o_dens: 16, 21 / 14468678, 0.00%
+    # o_dens: 17, 1 / 14468678, 0.00%
+    # o_dens: 18, 61 / 14468678, 0.00%
+    # o_dens: 19, 3 / 14468678, 0.00%
+    # average: 1.83
+
+    # number of simu-notes per bar
+    # b_dens: 1, 12998 / 665612, 1.95%
+    # b_dens: 2, 6042 / 665612, 0.91%
+    # b_dens: 3, 4340 / 665612, 0.65%
+    # b_dens: 4, 4978 / 665612, 0.75%
+    # b_dens: 5, 12847 / 665612, 1.93%
+    # b_dens: 6, 15565 / 665612, 2.34%
+    # b_dens: 7, 22219 / 665612, 3.34%
+    # b_dens: 8, 29463 / 665612, 4.43%
+    # b_dens: 9, 180284 / 665612, 27.09%
+    # b_dens: 10, 78078 / 665612, 11.73%
+    # b_dens: 11, 52481 / 665612, 7.88%
+    # b_dens: 12, 34962 / 665612, 5.25%
+    # b_dens: 13, 46884 / 665612, 7.04%
+    # b_dens: 14, 29673 / 665612, 4.46%
+    # b_dens: 15, 23716 / 665612, 3.56%
+    # b_dens: 16, 18494 / 665612, 2.78%
+    # b_dens: 17, 92588 / 665612, 13.91%
+    # average: 10.90
+
+    print("err_note_number_list", err_note_number_list)
+    print("err_beat_number_list", err_beat_number_list)
+    print("strength control error", np.mean(err_note_number_list) / 1.83)
+    print("density control error", np.mean(err_beat_number_list) / 10.90)
+
+
 def generate():
     # path
     from model_encoder import ModelForInference
 
     parser = argparse.ArgumentParser(description="Demo of argparse")
     parser.add_argument('-c', '--ckpt', default="../exp/loss_8_params.pt")
-    parser.add_argument('-f', '--files', default="../inference/vlog_short.npz")
+    parser.add_argument('-f', '--files', default="../inference/wzk.npz")
     args = parser.parse_args()
 
     path_saved_ckpt = args.ckpt
@@ -97,10 +146,8 @@ def generate():
                                                                           o_den_track_list=[1, 2, 3], pre_init=pre_init,
                                                                           C=C)
                     # res = net(None, condition=False)
-                    print("err_note_number_list", err_note_number_list)
-                    print("err_beat_number_list", err_beat_number_list)
-                    print("err_note_number_list", np.mean(err_note_number_list))
-                    print("err_beat_number_list", np.mean(err_beat_number_list))
+                    cal_control_error(err_note_number_list, err_beat_number_list)
+
 
                     # res = net(None, condition=True, vlog=vlog_npz, o_den_track_list=[1, 2, 3])
                     # result = res[:, [1, 0, 2, 3, 4, 5, 6]].astype(np.int32)
--- src/video2npz/metadata2numpy_mix.py
+++ src/video2npz/metadata2numpy_mix.py
@@ -122,7 +122,7 @@ def metadata2numpy(metadata):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--out_dir', default="../../inference/")
-    parser.add_argument('--video', default="vlog_short.mp4")
+    parser.add_argument('--video', default="wzk.mp4")
     parser.add_argument('--metadata', default="metadata.json")
     args = parser.parse_args()
 
